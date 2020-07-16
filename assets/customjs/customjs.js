08ba92efcbdde81cebb867bff08be611c80fa409
Shakil Ahmed <shakilatrai5@gmail.com>
2020-07-16 19:49:55 +0600

    Sidebar added

--- assets/customjs/customjs.js
+++ assets/customjs/customjs.js
@@ -2,27 +2,114 @@
 
 
 $(".home").click(function () {
-    $('.home').addClass('.active');
-    $('.hr-man').removeClass('.active');
-    $('.anthil-man').removeClass('.active');
-    $('.project-man').removeClass('.active');
-    $('.finance-man').removeClass('.active');
-    $('.lead-gen').removeClass('.active');
-    $('.content-man').removeClass('.active');
-    $('.client-man').removeClass('.active');
-    $('.presentation').removeClass('.active');
+    $('.home').addClass('active');
+    $('.hr-man').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.client-man').removeClass('active');
+    $('.presentation').removeClass('active');
 });
 
 
 $(".hr-man").click(function () {
-    console.log("clicked HR Man");
-    $('.hr-man').addClass('.active');
-    $('.home').removeClass('.active');
-    $('.anthil-man').removeClass('.active');
-    $('.project-man').removeClass('.active');
-    $('.finance-man').removeClass('.active');
-    $('.lead-gen').removeClass('.active');
-    $('.content-man').removeClass('.active');
-    $('.client-man').removeClass('.active');
-    $('.presentation').removeClass('.active');
+    $('.hr-man').addClass('active');
+    $('.home').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.client-man').removeClass('active');
+    $('.presentation').removeClass('active');
+});
+
+$(".anthil-man").click(function () {
+    $('.anthil-man').addClass('active');
+    $('.home').removeClass('active');
+    $('.hr-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.client-man').removeClass('active');
+    $('.presentation').removeClass('active');
+});
+
+
+$(".project-man").click(function () {
+    $('.project-man').addClass('active');
+    $('.home').removeClass('active');
+    $('.hr-man').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.client-man').removeClass('active');
+    $('.presentation').removeClass('active');
+});
+
+
+$(".finance-man").click(function () {
+    $('.finance-man').addClass('active');
+    $('.home').removeClass('active');
+    $('.hr-man').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.client-man').removeClass('active');
+    $('.presentation').removeClass('active');
+});
+
+
+$(".lead-gen").click(function () {
+    $('.lead-gen').addClass('active');
+    $('.home').removeClass('active');
+    $('.hr-man').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.client-man').removeClass('active');
+    $('.presentation').removeClass('active');
+});
+
+$(".content-man").click(function () {
+    $('.content-man').addClass('active');
+    $('.home').removeClass('active');
+    $('.hr-man').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.client-man').removeClass('active');
+    $('.presentation').removeClass('active');
+});
+
+
+$(".client-man").click(function () {
+    $('.client-man').addClass('active');
+    $('.home').removeClass('active');
+    $('.hr-man').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.presentation').removeClass('active');
+});
+
+$(".presentation").click(function () {
+    $('.presentation').addClass('active');
+    $('.home').removeClass('active');
+    $('.hr-man').removeClass('active');
+    $('.anthil-man').removeClass('active');
+    $('.project-man').removeClass('active');
+    $('.finance-man').removeClass('active');
+    $('.lead-gen').removeClass('active');
+    $('.content-man').removeClass('active');
+    $('.client-man').removeClass('active');
 });
